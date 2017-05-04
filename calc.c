e675ed60da5304b1e917d9b79dc18cb06c9cfc19
godic <shinshin11@pusan.ac.kr>
2017-05-04 00:40:51 -0700

    using function pointer

--- calc.c
+++ calc.c
@@ -3,31 +3,33 @@
 
 int main(){
 	FILE *fp = NULL;
-	int operand1, operand2;
+	double operand1, operand2;
 	char operator = ' ';
-	int result, line = 0;
+	double (*result)(double, double);
+	double line = 0;
 
 	fp = fopen("read.txt","r");
 	if(fp!=NULL){
-		fscanf(fp, "%d", &line);
+		fscanf(fp, "%lf", &line);
 	
 		for(int i=0; i<line; i++) {
-			fscanf(fp, "%d %c %d",&operand1, &operator, &operand2);
+			fscanf(fp, "%lf %c %lf",&operand1, &operator, &operand2);
 			switch(operator) {
 				case '+':
-				result = add(operand1, operator);
+				result = &add;
 				break;
 				case '-':
-				result = minus(operand1, operator);
+				result = &minus;
 				break;
 				case '*':
-				result = mul(operand1, operator);
+				result = &mul;
+				break;
 				case '/':
-				result = div(operand1, operator);
+				result = &div;
 				break;
 			}		
-			printf("%d %c %d = %d\n",
-				 operand1, operator, operand2, result);
+			printf("%.0lf %c %.0lf = %.6lf\n",
+				 operand1, operator, operand2, result(operand1, operand2));
 		}
 	}
 	return 0;
